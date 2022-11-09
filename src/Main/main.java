e3dc0052533894e591beedb834bb47ec426133ca
LDAJBS1 <LDAJBS1@PLDAM3116.mshome.net>
2022-11-09 20:04:09 +0100

    Alta clases Admin y vecino con sus metodos

--- src/Main/main.java
+++ src/Main/main.java
@@ -4,6 +4,7 @@
  */
 package Main;
 
+import usuario.Admin;
 import usuario.Vecino;
 /**
  *
@@ -15,11 +16,23 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Vecino Martin = new Vecino(1,"2","3","4","5","6");
-
-        System.out.println("hola mundo!3333");
-        System.out.println(Martin.toString());
+        //instancia de la clase Vecino 
+        Vecino vecino = new Vecino(1,"Martin","abc","666778899","martin@viu.es","vecino");
+        //instancia de la clase Admin 
+        Admin admin = new Admin(2,"Javier","abc","666778899","javier@viu.es","Administrador");
+        
+        //Imprimimos los valores de vecino
+        System.out.println(vecino.toString());
+        //Imprimimos el ID del usuario      
+        System.out.println("ID Usuario: " + vecino.getId());
+         //Imprimimos el rol del usuario      
+        System.out.println("Rol usuario: " + vecino.getRol());
+        //Imprimimos la validacion "token" del login del usuario      
+        System.out.println("Login: " + vecino.login(1,"KK","abc"));
+        //Imprimimos la validacion "token" del login del usuario      
+        System.out.println("Modificacion usuario: " + admin.modUsuario(2,"Javier","abc","666778899","javier@viu.es","Administrador"));
+        
+        
     }
     
 }
