e15ab5d243b63c6a0b63e5082ca3d7cf580e7323
fersie7e <117305511+fersie7e@users.noreply.github.com>
2022-11-28 13:06:53 +0100

    Reunion Grupo 3 28/11/22 13:00 Asistentes:	-Fernando 			-Javier 			-Martin Modificaciones: 	- Creacion del javadoc de la clase VecinoInterface

--- src/usuario/VecinoInterface.java
+++ src/usuario/VecinoInterface.java
@@ -18,10 +18,35 @@ import repositorio.Liquidacion;
  */
 public interface VecinoInterface {
     
-
+    /** Metodo abstracto de la clase vecino usado por el admin para dar de baja a un vecino
+     * 
+     * @param comunidadCRUD
+     * @param vecinoBorrar 
+     */
     public abstract void bajaVecino (ComunidadCRUD comunidadCRUD, Vecino vecinoBorrar);
+    
+    /** Metodo abstracto de la clase vecino usado por el admin para actualizar los datos del vecino
+     * 
+     * @param comunidadCRUD
+     * @param nombre
+     * @param clave
+     * @param telefono
+     * @param email
+     * @param vecinoModificar 
+     */
     public abstract void updateVecino (ComunidadCRUD comunidadCRUD, String nombre, String clave, String telefono, String email, Vecino vecinoModificar);
+    
+    /** Metodo abstracto usado por la clase vecino para consultar sus liquidaciones
+     * 
+     * @param comunidadCRUD
+     * @param liquidacion 
+     */
     public abstract void consultarLiquidacion (ComunidadCRUD comunidadCRUD, Liquidacion liquidacion);
+    
+    /** Metodo abstracto usado por la clase vecino para consultar los servicios a los que estan adscritos sus inmuebles
+     * 
+     * @param comunidadCRUD 
+     */
     public abstract void consultarServicioCuentas (ComunidadCRUD comunidadCRUD);
     
 }
